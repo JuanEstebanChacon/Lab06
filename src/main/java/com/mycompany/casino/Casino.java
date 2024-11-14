84c984787712fc34742996e3e9d10443037bcd3e
JuanEstebanChacon <est.juane.chacon@unimilitar.edu.co>
2024-11-14 18:39:50 -0500

    Add files via upload

--- src/main/java/com/mycompany/casino/Casino.java
+++ src/main/java/com/mycompany/casino/Casino.java
@@ -9,12 +9,16 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Casino extends JFrame {
     private JPanel mainPanel;
     private JButton botondados;
     private JButton botonmonedas;
-
+    private ImageIcon[] images = new ImageIcon[6];
+   
     public Casino() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -22,6 +26,12 @@ public class Casino extends JFrame {
         mainPanel = new JPanel();
         botondados = new JButton("Dados");
         botonmonedas = new JButton("Traga Monedas");
+        for (int n = 0; n < 6; n++) {
+            ImageIcon originalImage = new ImageIcon("C:\\Users\\juche\\Documents\\NetBeansProjects\\casino\\src\\main\\java\\com\\mycompany\\casino\\Imagenes\\" + (n + 1) + ".jpg");
+            Image scaledImage = originalImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            images[n] = new ImageIcon(scaledImage);
+        }
+
 
         mainPanel.add(botondados);
         mainPanel.add(botonmonedas);
@@ -44,102 +54,121 @@ public class Casino extends JFrame {
     }
 
     class PanelDados extends JPanel {
-        private ImageIcon NumeroDados[] = new ImageIcon[6];
-        private JLabel Dado1 = new JLabel(NumeroDados[0], JLabel.CENTER);
-        private JLabel Dado2 = new JLabel(NumeroDados[0], JLabel.CENTER);
+      
+
         public PanelDados() {
             setLayout(new BorderLayout());
-            JLabel label = new JLabel("", JLabel.CENTER);
+            JLabel label = new JLabel("Juego de Dados", JLabel.CENTER);
             JButton volverButton = new JButton("Volver");
-            JButton iniciarrButton = new JButton("Iniciar");
-            JPanel imagePanel = new JPanel();
-            imagePanel.setLayout(new GridLayout(1, 2, 10, 10));
-            imagePanel.add(Dado1);
-            imagePanel.add(Dado2);
-
-            add(label, BorderLayout.NORTH);
-            add(imagePanel, BorderLayout.CENTER);
+            JButton iniciarButton = new JButton("Iniciar");
             
             
             
-            add(label, BorderLayout.CENTER);
             JPanel buttonPanel = new JPanel();
             buttonPanel.add(volverButton);
-            buttonPanel.add(iniciarrButton);
+            buttonPanel.add(iniciarButton);
             add(buttonPanel, BorderLayout.SOUTH);
-
             volverButton.addActionListener((ActionEvent e) -> {
-                ventanas(mainPanel); 
+                ventanas(mainPanel);
             });
-            iniciarrButton.addActionListener((ActionEvent e) -> {
-                ventanas(mainPanel); 
+
+            iniciarButton.addActionListener((ActionEvent e) -> {
+
             });
         }
     }
+ 
 
     class PanelMonedas extends JPanel {
-        private ImageIcon Imagenes[] = new ImageIcon[7];
-        private JLabel Imagen1 = new JLabel(Imagenes[0], JLabel.CENTER);
-        private JLabel Imagen2 = new JLabel(Imagenes[0], JLabel.CENTER);
-        private JLabel Imagen3 = new JLabel(Imagenes[0], JLabel.CENTER);
+        private JLabel Imagen1;
+        private JLabel Imagen2;
+        private JLabel Imagen3;
         public PanelMonedas() {
             setLayout(new BorderLayout());
             JLabel label = new JLabel("", JLabel.CENTER);
             JButton volverButton = new JButton("Volver");
             JButton iniciarrButton = new JButton("Iniciar");
-            JPanel imagePanel = new JPanel();
-            imagePanel.setLayout(new GridLayout(1, 2, 10, 10));
-            Imagenes[0] = new ImageIcon(getClass().getResource("/casino/Imagenes/1.jpg"));
-            Imagenes[1] = new ImageIcon(getClass().getResource("/casino/Imagenes/2.jpg"));
-            Imagenes[2] = new ImageIcon(getClass().getResource("/casino/Imagenes/3.jpg"));
-            Imagenes[3] = new ImageIcon(getClass().getResource("/casino/Imagenes/4.jpg"));
-            Imagenes[4] = new ImageIcon(getClass().getResource("/casino/Imagenes/5.jpg"));
-            Imagenes[5] = new ImageIcon(getClass().getResource("/casino/Imagenes/6.jpg"));
-            Imagenes[6] = new ImageIcon(getClass().getResource("/casino/Imagenes/7.jpg"));
-            
-            Imagen1.setIcon(Imagenes[0]);
-            Imagen2.setIcon(Imagenes[0]);
-            Imagen3.setIcon(Imagenes[0]);
+
+            JPanel imagePanel = new JPanel(new GridLayout(1, 2, 10, 10));
+
+            Imagen1 = new JLabel(images[0], JLabel.CENTER);
+            Imagen2 = new JLabel(images[1], JLabel.CENTER);
+            Imagen3 = new JLabel(images[2], JLabel.CENTER);
+
             imagePanel.add(Imagen1);
             imagePanel.add(Imagen2);
             imagePanel.add(Imagen3);
-         
+
             add(label, BorderLayout.NORTH);
             add(imagePanel, BorderLayout.CENTER);
-            
-            
-            
-            add(label, BorderLayout.CENTER);
+
             JPanel buttonPanel = new JPanel();
             buttonPanel.add(volverButton);
             buttonPanel.add(iniciarrButton);
             add(buttonPanel, BorderLayout.SOUTH);
 
+            add(label, BorderLayout.NORTH);
+            add(imagePanel, BorderLayout.CENTER);
+
             volverButton.addActionListener((ActionEvent e) -> {
-                ventanas(mainPanel); 
+                ventanas(mainPanel);
             });
             iniciarrButton.addActionListener((ActionEvent e) -> {
-                ventanas(mainPanel); 
+             Thread hilo = new Thread(new cambiarimagenes());
+             hilo.start();
+             Thread hilo2 = new Thread(new cambiarimagenes2());
+             hilo2.start();
+             Thread hilo3 = new Thread(new cambiarimagenes3());
+             hilo3.start();
             });
+
         }
-        private class RuletaImagen1 implements Runnable {
-        
+         private class cambiarimagenes implements Runnable {
             @Override
             public void run() {
                 try {
                     for (int i = 0; i < 10; i++) {
-                        int randomIndex = (int) (Math.random() * Imagenes.length); // Genera un índice aleatorio para la cara
-                        Imagen1.setIcon(Imagenes[randomIndex]); // Actualiza la imagen de la cara del dado
+                        int randomIndex = (int) (Math.random() * images.length); // Genera un índice aleatorio para la cara
+                        Imagen1.setIcon(images[randomIndex]); // Actualiza la imagen de la cara del dado
                         mainPanel.repaint();
                         Thread.sleep(200); // Pausa de 200 ms
                     }
                 } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+         private class cambiarimagenes2 implements Runnable {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        int randomIndex = (int) (Math.random() * images.length); // Genera un índice aleatorio para la cara
+                        Imagen2.setIcon(images[randomIndex]); // Actualiza la imagen de la cara del dado
+                        mainPanel.repaint();
+                        Thread.sleep(200); // Pausa de 200 ms
+                    }
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+         private class cambiarimagenes3 implements Runnable {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        int randomIndex = (int) (Math.random() * images.length); // Genera un índice aleatorio para la cara
+                        Imagen3.setIcon(images[randomIndex]); // Actualiza la imagen de la cara del dado
+                        mainPanel.repaint();
+                        Thread.sleep(200); // Pausa de 200 ms
+                    }
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
                 }
             }
         }
     }
-    
-
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
